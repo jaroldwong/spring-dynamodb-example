c4a7761b8579c9f14003863b81ab5a0ecf6439fb
Jarold Wong <cwjwong@ucdavis.edu>
2019-05-02 17:14:47 -0700

    Attempt query using DBmapper

--- src/main/java/logger/EventService.java
+++ src/main/java/logger/EventService.java
@@ -4,10 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 @Service
 public class EventService {
+
+    @Autowired
+    DynamoDBMapper mapper;
 
     @Autowired
     private EventRepository eventRepository;
@@ -21,7 +30,15 @@ public class EventService {
     }
 
     public List<Event> getEventsByLogEntityId(String logEntityId) {
-        return eventRepository.findByLogEntityId(logEntityId);
+        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":val1", new AttributeValue().withS(logEntityId));
+
+        DynamoDBQueryExpression<Event> queryExpression = new DynamoDBQueryExpression<Event>()
+                .withKeyConditionExpression("logEntityId = :val1").withExpressionAttributeValues(eav);
+
+        List<Event> events = mapper.query(Event.class, queryExpression);
+
+        return events;
     }
 
     public void addEvent(Event event) {
